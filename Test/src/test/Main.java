a73c5fb84f259b370f1f4a6f67a671be59bbce03
KALA2 <KaloyanC@KaloyanC-PC.grafixoft.com>
2013-03-14 13:27:19 +0200

    1234

--- Test/src/test/Main.java
+++ Test/src/test/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub1234kala
+		// TODO Auto-generated method stub1234kala1234
 		System.out.print("alabala");
 	}
 
